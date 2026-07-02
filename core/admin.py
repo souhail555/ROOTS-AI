d342b725fb7c490f014da3f3a2bc3bd178071e52
Abi Dzar <mail.dzar@gmail.com>
2026-07-02 12:18:00 +0800

    add section in admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .models import User
+
+
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (
+        (None, {'fields': ('role',)}),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
